1e46afc5700e6a03db1a816941b9d6d1c59acc76
Julian Dühnen <j.b.duhnen@student.utwente.nl>
2024-12-19 15:53:56 +0100

    Progress

--- utils/mesh_tools.py
+++ utils/mesh_tools.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+import numpy as np
 import open3d as o3d
 
 
@@ -17,3 +18,14 @@ def create_quad(
     )
     mesh.triangles = o3d.utility.Vector3iVector([[0, 1, 2], [0, 2, 3]])
     return mesh
+
+
+def normalize_mesh(mesh):
+    vertices = np.asarray(mesh.vertices)
+    min_bounds = vertices.min(axis=0)
+    max_bounds = vertices.max(axis=0)
+    center = (min_bounds + max_bounds) / 2
+    scale = max_bounds - min_bounds
+    vertices -= center  # Center
+    vertices /= scale.max()
+    mesh.vertices = o3d.utility.Vector3dVector(vertices)  # Normalize
