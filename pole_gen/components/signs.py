Fixed pole radius at calculation

pole_gen/components/signs.py
@@ -183,15 +183,15 @@ def _add_small_rectangular_side_signs(state: State):
             possible_rotations = []
             if state.is_intersection:
                 possible_rotations = [
-                    np.deg2rad(45),
-                    np.deg2rad((90 * state.rot_indices[state.main_road]) + 45),
-                    np.deg2rad((180 * state.rot_indices[state.main_road]) + 45),
-                    np.deg2rad((270 * state.rot_indices[state.main_road]) + 45),
+                    np.deg2rad(-45),
+                    np.deg2rad((90 * state.rot_indices[state.main_road]) - 45),
+                    np.deg2rad((180 * state.rot_indices[state.main_road]) - 45),
+                    np.deg2rad((270 * state.rot_indices[state.main_road]) - 45),
                 ]
             else:
                 possible_rotations = [
-                    np.deg2rad(45),
-                    np.deg2rad((180 * state.rot_indices[state.main_road]) + 45),
+                    np.deg2rad(-45),
+                    np.deg2rad((180 * state.rot_indices[state.main_road]) - 45),
                 ]
             np.random.shuffle(possible_rotations)
 

pole_gen/models.py
@@ -89,7 +89,7 @@ class State:
         return np.interp(
             height,
             [0, self.pole_scaled_height],
-            [self.pole_top_radius, self.pole_base_radius],
+            [self.pole_base_radius, self.pole_top_radius],
         )
 
     def add_geometry(self, mesh: o3d.geometry.TriangleMesh, label: UtilityPoleLabel):